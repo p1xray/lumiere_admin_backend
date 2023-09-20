09c170db788a13c64008c1bae59e6e1f95675acc
LeanSlv <youoliver@mail.ru>
2023-09-20 19:06:31 +0200

    feat(config): Чтение переменных окружения в конфиг для БД

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,13 +3,20 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 // Конфиг
 type Config struct {
+	// Адрес, на котором запускается сервер
 	HttpAddr string
+	// Строка подключения к БД
+	DatabaseUrl string
+
+	// Количество пул соединений
+	PoolMax int
 }
 
 // Читает конфиг из переменных окружения
@@ -19,9 +26,23 @@ func Read() Config {
 	if err := godotenv.Load("local.env"); err != nil {
 		log.Fatalf("Config Read Error: %v", err)
 	}
+
 	httpAddr, exists := os.LookupEnv("HTTP_ADDR")
 	if exists {
 		config.HttpAddr = httpAddr
+	}
+
+	dbUrl, exists := os.LookupEnv("DB_URL")
+	if exists {
+		config.DatabaseUrl = dbUrl
+	}
+
+	poolMaxStr, exists := os.LookupEnv("POOL_MAX")
+	if exists {
+		poolMax, err := strconv.Atoi(poolMaxStr)
+		if err == nil {
+			config.PoolMax = poolMax
+		}
 	}
 
 	return config
